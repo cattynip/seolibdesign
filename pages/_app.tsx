aeb220bbe1a370af46196bf49db57dd71c590bd2
cattynip <cattynip.cattynip@gmail.com>
2022-11-12 16:14:30 -0800

    style(theme and fonts): change theme and fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@components/Header';
+import theme from '@libs/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
     </ChakraProvider>
