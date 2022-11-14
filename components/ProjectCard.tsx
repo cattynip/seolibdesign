6f8c56e888ec3869bf911a5d7366ba13a9dffdeb
cattynip <cattynip.cattynip@gmail.com>
2022-11-13 16:20:22 -0800

    feat(component): project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,11 +2,12 @@ import {
   Heading,
   Img,
   Container,
-  Flex,
+  Box,
+  Text,
+  Link,
   useColorModeValue,
-  Text
+  Flex
 } from '@chakra-ui/react';
-import LinkItem from './LinkItem';
 
 export interface IProjectCard {
   title: string;
@@ -24,26 +25,28 @@ const ProjectCard = ({
   const boxBgColor = useColorModeValue('#383831', '#f0e7db');
   const textColor = useColorModeValue('white', 'black');
 
-  const titleText = title.charAt(0).toUpperCase() + title.slice(1);
-
   return (
-    <LinkItem href={linkTo}>
-      <Flex
-        bg={boxBgColor}
-        width="container.sm"
-        maxW="90%"
-        textColor="black"
-        borderRadius="lg"
-      >
-        <Img height={250} m={0} borderLeftRadius="lg" src={imageUrl}></Img>
-        <Container px={5} pt={5} experimental_spaceY={1}>
-          <Heading as="h2" size="xl" textColor={textColor}>
-            {titleText}
-          </Heading>
-          <Text textColor={textColor}>{description}</Text>
+    <Link
+      href={linkTo}
+      width={{ md: 'container.md', sm: '100%' }}
+      bg={boxBgColor}
+      textColor={textColor}
+      borderRadius="lg"
+      style={{ textDecoration: 'none' }}
+    >
+      <Box as={Flex}>
+        <Img
+          src={imageUrl}
+          borderLeftRadius="lg"
+          height={250}
+          display={{ md: 'block', sm: 'none' }}
+        />
+        <Container p={4}>
+          <Heading as="h2">{title}</Heading>
+          <Text>{description}</Text>
         </Container>
-      </Flex>
-    </LinkItem>
+      </Box>
+    </Link>
   );
 };
 
