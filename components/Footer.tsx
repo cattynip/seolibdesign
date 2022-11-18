56198c6a826d62a40b48aa0ca37a37b94e741996
cattynip <cattynip.cattynip@gmail.com>
2022-11-17 23:16:02 -0800

    feat(component): footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Box, useColorModeValue } from '@chakra-ui/react';
+
+const Footer = () => {
+  const bgColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
+
+  return (
+    <Box mt={10} py={5} bgColor={bgColor} textAlign="center">
+      &copy; {new Date().getFullYear()} Seol SO. Al Rights Reserved.
+    </Box>
+  );
+};
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app';
 import Header from '@components/Header';
 import theme from '@libs/theme';
 import { ChakraProvider } from '@chakra-ui/react';
+import Footer from '@components/Footer';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
