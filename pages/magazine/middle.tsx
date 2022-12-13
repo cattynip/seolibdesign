aa95e8e2790f8b67d45be41b5e9b86a9897365d9
cattynip <cattynip.cattynip@gmail.com>
2022-12-12 20:29:08 -0800

    refactor(routing): changed method to route

---
+++ pages/magazine/middle.tsx
@@ -0,0 +1,43 @@
+import { NextPage } from 'next';
+import DetailedLayout from '@components/layouts/DetailedLayout';
+import { DataObjectMiddle } from '@data/data';
+
+const MagazineMiddle: NextPage = () => {
+  const data = DataObjectMiddle;
+
+  return (
+    <DetailedLayout
+      title={data.name}
+      description={data.description}
+      section={data.kind}
+      datas={{
+        initial: {
+          explaination: data.initial,
+          urls: [
+            '/middle/1-1-1.jpeg',
+            '/middle/1-1-2.jpeg',
+            '/middle/1-2-1.jpeg',
+            '/middle/1-2-2.jpeg',
+            '/middle/1-3-1.jpeg',
+            '/middle/1-3-2.jpeg'
+          ]
+        },
+        intermediate: {
+          explaination: data.intermediate,
+          urls: [
+            '/middle/2-1-1.jpeg',
+            '/middle/2-1-2.jpeg',
+            '/middle/2-2-1.jpeg',
+            '/middle/2-2-2.jpeg'
+          ]
+        },
+        final: {
+          explaination: data.final,
+          urls: ['/middle/3-1.jpeg', '/middle/3-2.jpeg']
+        }
+      }}
+    />
+  );
+};
+
+export default MagazineMiddle;
