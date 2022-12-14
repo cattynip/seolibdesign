8cbcc81cbab73c19db35498d5a54c4f804287c8a
cattynip <cattynip.cattynip@gmail.com>
2022-12-14 01:04:14 -0800

    refactor(component): remove detailed image

--- components/detailed/DetailedTree.tsx
+++ components/detailed/DetailedTree.tsx
@@ -1,18 +1,13 @@
 import { Box, Container, Heading, useColorModeValue } from '@chakra-ui/react';
 import Description from '@components/Description';
-import { TDataType } from '@data/data';
-import DetailedImage from './DetailedImage';
 
 interface IDetailedTree {
   stageType: TDataType;
-  images: string[];
-  double?: boolean;
 }
 
 const DetailedTree = ({
   stageType,
   images,
-  double,
   ...props
 }: IDetailedTree & BoxProps) => {
   return (
@@ -38,11 +33,6 @@ const DetailedTree = ({
         alignItems="center"
         justifyContent="space-around"
       >
-        {stageType === 'intermediate' ? <Box /> : null}
-        {images.map(image => (
-          <DetailedImage key={image} imageData={[image]} />
-        ))}
-        {stageType === 'intermediate' ? <Box /> : null}
       </Box>
     </Box>
   );
