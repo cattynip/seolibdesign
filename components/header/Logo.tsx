196620c3e20478dcad204debdfde2455026ab55a
cattynip <cattynip.cattynip@gmail.com>
2022-12-17 00:33:45 -0800

    refactor(image): apply images

--- components/header/Logo.tsx
+++ components/header/Logo.tsx
@@ -1,21 +1,12 @@
-import { Text, useColorModeValue } from '@chakra-ui/react';
+import { Img, Text, useColorModeValue } from '@chakra-ui/react';
 import Link from 'next/link';
 
 const Logo = () => {
+  const iconSrc = useColorModeValue('/logo-light.svg', '/logo-dark.svg');
+
   return (
     <Link href="/">
-      <Text
-        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-        align="center"
-        px={2}
-        pr={8}
-        justifyContent="center"
-        fontSize={19}
-        fontFamily='M PLUS Rounded 1c", "sans-serif'
-        fontWeight="bold"
-      >
-        Seol IB Design
-      </Text>
+      <Img src={iconSrc} width={'10'} borderRadius="full" />
     </Link>
   );
 };
